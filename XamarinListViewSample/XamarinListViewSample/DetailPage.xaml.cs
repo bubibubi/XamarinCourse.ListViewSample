28cb21909b9e4cb7b5b31f200095f4036b109d0d
bubibubi <u.ballestrazzi@gmail.com>
2017-02-27 19:46:19 +0100

    Add project files.

---
+++ XamarinListViewSample/XamarinListViewSample/DetailPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinListViewSample
+{
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public DetailPage(string id) : this()
+        {
+            lblName.Text = id;
+        }
+    }
+}
---
+++ XamarinListViewSample/XamarinListViewSample/ListViewPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinListViewSample
+{
+    public partial class ListViewPage : ContentPage
+    {
+        public ListViewPage()
+        {
+            InitializeComponent();
+
+            string[] items = new []
+            {
+                "Mario",
+                "Giovanni",
+                "Matteo",
+                "Carlo",
+                "Giacomo",
+                "Simone",
+                "Nando",
+                "Fabio",
+                "Martina",
+                "Luisa",
+                "Anna",
+                "Paola",
+                "Simone",
+                "Nando",
+                "Fabio",
+                "Simone",
+                "Nando",
+                "Fabio",
+                "Federica"
+            };
+
+            MyListView.ItemsSource = items;
+            MyListView.ItemTapped += MyListView_ItemTapped;
+        }
+
+        private void MyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((NavigationPage)App.Current.MainPage).PushAsync(new DetailPage(e.Item.ToString()));
+        }
+    }
+}
